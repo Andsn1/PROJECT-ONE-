17f56cf071b31b377935cc19ca75406007ae77f6
Andsn1 <152194109+Andsn1@users.noreply.github.com>
2024-02-07 20:43:48 +0000

    created traffic advice js file

---
+++ assets/scripts/trafficadvice.js
@@ -0,0 +1,61 @@
+// Need to create a variable for all the cities
+var trafficLocation = [London, Birmingham, Leeds, Edinburgh, sheffield];
+
+// need to create a variable for comments to appear if a city is selected.
+const lond1 = {
+  road1:
+    "london has one of the most congested roads, the highest of them being,A219 S/B from A304 Fulham Road to A297 Morden Hall Road.the peak hour being 17:00 with 12 minutes lost in traffic.",
+
+  road2:
+    "A202E/B from Neathouse Place to B215Peckham Hill Street.The peak hour 17:00 with 11 minutes lost in traffic.",
+
+  road3:
+    "A406 North Circular Road E/B from A1 Falloden Way to A110 Bowes Road.The peak hour being 16:00 with 11 minutes lost in traffic.",
+
+  road5:
+    "A24 S/B from A205 The Avenue to A238 Merton High Street.The peak hour being 16:00  with 10 minutes lost in traffic.",
+
+  road6:
+    "A205 E/B from A215 Norwood Road to Ravensbourne Road.The peak hour being 16:00 with 9 minutes lostin traffic",
+};
+
+Birm1 = {
+  road7:
+    "A45 E/B from Bordesley Circus to Henry Road.The peak hour being 16:00 with 9 minutes lost in traffic.",
+
+  road8:
+    "A435 S/B from Haden Circus to Wynfield.The peak hour being 16:00 with 8 minutes lost in traffic.",
+
+  road9:
+    "A45 W/B from B425 Hobs Moat Road to Bordesley Circus.The peak hour being 16:00 with 7 minutes lost in traffic.",
+
+  road10:
+    "A34 Stratford Road S/B from Camp Hill Circus to Hamlet Road.The peak hour being 17:00 with 7 minutes lost in traffic",
+};
+
+Leeds1 = {
+  road11:
+    "A6177 S/B from Bolton Road to Great Horton Road. The peak hourbeing 16:00 with 8 minutes lost in traffic.",
+
+  road12:
+    "A65 S/B from B6153 Park Road to The White Horse Roundabout.The peak hour being 16:00 iwith 7 minutes lost in traffic.",
+};
+
+Edin1 = {
+  road13:
+    "A902 W/B from A901 Great Junction Street to A90 Hillhouse Road. The peak hour being 16:00 with 7 minutes lost in traffic.",
+
+  road14:
+    "A702 S/B from Brougham Street to the City of Edinburgh Bypass.	The peak hour being 16:00 with 7 minutes lost in traffic.",
+};
+shef1 = {
+  road15:
+    "A61 from Moore Street Roundabout to A6101 Bradfield Road. The peak hour being 16:00 with 7 minutes lost in traffic.",
+};
+
+//need to console log when a city is selected.
+function selectLondon() {
+  console.log(lond1);
+}
+
+// if a city is sellected console log it data if not console log standard text.
